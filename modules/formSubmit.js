00b82b7dff1dbffc90272665ec26c1d9df5d1491
Theophileaseh <theophilenwachan@gmail.com>
2022-08-09 09:14:38 +0100

    Add dependency

--- modules/formSubmit.js
+++ modules/formSubmit.js
@@ -2,7 +2,6 @@ import { booksArray, addBook } from './storage.js';
 
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
-
 const formSubmit = document.querySelector('.book-form');
 
 formSubmit.addEventListener('submit', (event) => {
